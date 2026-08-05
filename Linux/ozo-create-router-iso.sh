582ee2494a8bc527a8615a6b2410d5843a31e420
Andy Lievertz <alievertz@onezeroone.dev>
2026-08-04 20:58:08 -0600

    save commit

--- Linux/ozo-create-router-iso.sh
+++ Linux/ozo-create-router-iso.sh
@@ -4,31 +4,45 @@
 #TARGET_ISO_PATH="/mnt/c/ozo-ad-lab/ISO/OZO-AD-Lab-Router.iso"
 #TARGET_ISO_LABEL="OZO-AD-Lab-Router"
 
-# wsl --distribution "Debian" --user root KICKSTART_PATH="/mnt/c/ozo-ad-lab/Linux/ozo-ad-lab-router-ks.cfg" SOURCE_ISO_PATH="/mnt/c/ozo-ad-lab/ISO/almalinux-boot.iso" TARGET_ISO_PATH="/mnt/c/ozo-ad-lab/ISO/OZO-AD-Lab-Router.iso" TARGET_ISO_LABEL="OZO-AD-Lab-Router" /mnt/c/ozo-ad-lab/Linux/ozo-create-router-iso.sh
+# wsl --distribution "Debian" --user root GRUB_PATH="/mnt/c/ozo-ad-lab/Linux/grub.cfg" KICKSTART_PATH="/mnt/c/ozo-ad-lab/Linux/ozo-ad-lab-router-ks.cfg" SOURCE_ISO_PATH="/mnt/c/ozo-ad-lab/ISO/almalinux-boot.iso" TARGET_ISO_PATH="/mnt/c/ozo-ad-lab/ISO/OZO-AD-Lab-Router.iso" TARGET_ISO_LABEL="OZO-AD-Lab-Router" /mnt/c/ozo-ad-lab/Linux/ozo-create-router-iso.sh
 
 # Local variables
 OZO_AD_LAB_PATH=~/ozo-ad-lab
 MNT_PATH=$OZO_AD_LAB_PATH/mnt
 COPY_PATH=$OZO_AD_LAB_PATH/router
 
+# Fail if the Grub configuration does not exist
+if [[ ! -f $GRUB_PATH ]]
+then
+    echo "Grub configuration file not found."
+    exit 0
+fi
+
+# Fail if the Kickstart does not exist
+if [[ ! -f $KICKSTART_PATH ]]
+then
+    echo "Kickstart file not found."
+    exit 0
+fi
+
 # Fail if the source ISO does not exist
 if [[ ! -f $SOURCE_ISO_PATH ]]
 then
-    echo "FALSE"
+    echo "Source ISO path is not set."
     exit 0
 fi
 
 # Fail if target ISO path is not set
 if [[ $TARGET_ISO_PATH = "" ]]
 then
-    echo "FALSE"
+    echo "Target ISO path is not set."
     exit 0
 fi
 
 # Fail if target ISO label is not set
 if [[ $TARGET_ISO_LABEL = "" ]]
 then
-    echo "FALSE"
+    echo "Target ISO label is not set."
     exit 0
 fi
 
@@ -55,20 +69,16 @@ rsync -av $MNT_PATH/ $COPY_PATH/ >/dev/null 2>&1
 # Unmount the ISO
 umount $MNT_PATH >/dev/null 2>&1
 # Copy in the Kickstart
-cp $KICKSTART_PATH $COPY_PATH/
-# Modify the grub boot menu entries
-sed -i '0,/\}/!{0,/\}/!d}' $COPY_PATH/EFI/BOOT/grub.cfg
-sed -i 's/set default="1"/set default="0"/' $COPY_PATH/EFI/BOOT/grub.cfg
-sed -i 's/set timeout=60/set timeout=0/' $COPY_PATH/EFI/BOOT/grub.cfg
-sed -i 's/.*linuxefi.*/& ip=eth0:dhcp inst.text inst.ks=cdrom:\/ozo-ad-lab-router-ks.cfg/g' $COPY_PATH/EFI/BOOT/grub.cfg
-# Modify the isolinux boot menu entries
-sed -i '0,/append/!d' $COPY_PATH/isolinux/isolinux.cfg
-sed -i 's/timeout 600/timeout 0/' $COPY_PATH/isolinux/isolinux.cfg
-sed -i 's/.*append.*/& ip=eth0:dhcp inst.text inst.ks=cdrom:\/ozo-ad-lab-router-ks.cfg/g' $COPY_PATH/isolinux/isolinux.cfg
+cp $KICKSTART_PATH $COPY_PATH/ks.cfg
+# Copy in grub configuration
+cp -f $GRUB_PATH $COPY_PATH/EFI/BOOT/grub.cfg
+cp -f $GRUB_PATH $COPY_PATH/boot/grub2/grub.cfg
 # Create the modified ISO
-mkisofs -b isolinux/isolinux.bin -J -R -l -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -eltorito-alt-boot -e images/efiboot.img -no-emul-boot -graft-points -V $TARGET_ISO_LABEL -o $TARGET_ISO_PATH $COPY_PATH/ >/dev/null 2>&1
+# mkisofs -input-charset utf-8 -b isolinux/isolinux.bin -J -R -l -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -eltorito-alt-boot -e images/efiboot.img -no-emul-boot -graft-points -V $TARGET_ISO_LABEL -o $TARGET_ISO_PATH $COPY_PATH/ >/dev/null 2>&1
+mkisofs -input-charset utf-8 -J -l -R -allow-multidot -iso-level 3 -udf -no-emul-boot -eltorito-boot images/eltorito.img -V $TARGET_ISO_LABEL -o $TARGET_ISO_PATH $COPY_PATH/
+# >/dev/null 2>&1
 # Make the ISO writable to USB
-isohybrid --uefi $TARGET_ISO_PATH >/dev/null 2>&1
+#isohybrid --uefi $TARGET_ISO_PATH >/dev/null 2>&1
 # Embed the MD5SUM
 implantisomd5 $TARGET_ISO_PATH >/dev/null 2>&1
 # Clean up
